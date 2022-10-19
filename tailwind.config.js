98baad7a0c6273fb0178a07c443d731377f7fa0a
Kehinde Malagu <Kehindemalagu@gmail.com>
2022-10-19 15:06:51 +0100

    Html Completed. Next is Integrating Tailwind Utility Classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/index.html", "/script.js"],
+  content: ["./index.html", "./script.js"],
   theme: {
     extend: {
       colors: {
